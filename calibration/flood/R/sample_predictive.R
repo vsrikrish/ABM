bc1090f0196eccddc90d1190a65ae113d82a74dd
vsrikrish <vxs914@psu.edu>
2020-11-13 10:15:09 -0500

    added sampling by seed

--- calibration/flood/R/sample_predictive.R
+++ calibration/flood/R/sample_predictive.R
@@ -1,7 +1,7 @@
 # This script plots hindcasts for the flood vacancy agent-based model
 
 library(reticulate) # interface with Python
-library(ncdf4)  # interface with netcdf files
+library(extRemes)
 library(reshape2)
 library(pryr)
 library(foreach)
@@ -9,16 +9,14 @@ library(doParallel)
 
 source('R/utils.R')
 
+use_condaenv("mcmc", conda='~/work/miniconda3/bin/conda', required = TRUE)
+
 set.seed(1234)
 
 # set number of prior and posterior samples
 nsamp <- 50000
 
 args <- commandArgs(trailingOnly=TRUE)
-lik_type <- args[1]
-
-data_path <- 'data'
-out_path <- file.path('output', paste0('model-id-', lik_type))
 
 # if PBS array used, get job number and specify parameters
 # otherwise get arguments from command call
@@ -28,22 +26,28 @@ if (aid != '') {
   n_yrs <- c(10, 25, 50)
   n_rows <- c(5, 10)
   n_cols <- c(5, 10)
-  priors <- c('informed', 'weakly')
   models <- c('simple', 'complex')
+  liks <- c('ind', 'agg')
   
-  pcases <- expand.grid(yr=n_yrs, cols=n_cols, rows=n_rows, prior=priors, model=models)
+  pcases <- expand.grid(yr=n_yrs, cols=n_cols, rows=n_rows, model=models, lik=liks, seed=1:5)
   yrs <- pcases[id, 'yr']
   rows <- pcases[id, 'rows']
   cols <- pcases[id, 'cols']
-  prior <- pcases[id, 'prior']
   model <- pcases[id, 'model']
+  seed <- pcases[id, 'seed']
+  lik_type <- pcases[id, 'lik']
+  print(pcases[id,])
 } else {
-  yrs <- as.numeric(args[2])
-  rows <- as.numeric(args[3])
-  cols <- as.numeric(args[4])
-  prior <- args[5]
-  model <- args[6]
+  yrs <- as.numeric(args[1])
+  rows <- as.numeric(args[2])
+  cols <- as.numeric(args[3])
+  model <- args[4]
+  seed <- as.numeric(args[5])
+  lik_type <- args[6]
 }
+
+data_path <- 'data'
+out_path <- file.path('output', paste0('model-id-', lik_type))
 
 start_year <- 2018-yrs+1
 end_year <- 2018
@@ -52,14 +56,21 @@ mem_length <- 10
 # load positive control data
 pcout_file <- file.path(data_path, 'model-id',
                           paste('pcout-', yrs, '-',
-                                  rows, '-', cols, '.nc',
+                                  rows, '-', cols, '-', seed, '.rds',
                                   sep = ''
                                )
                         )
-f <- nc_open(pcout_file)
-elev <- ncvar_get(f, 'elev')
-river_hist <- ncvar_get(f, 'water_height')
-nc_close(f)
+f <- readRDS(pcout_file)$flood_freq
+# generate water height history and housing elevations
+slope <- list('offset' = 890, 'mean' = 10)
+elev <- t(slope$offset +
+            slope$mean * matrix(seq(0, rows - 1),
+                                ncol = cols,
+                                nrow = rows
+            ) +
+            rnorm(cols * rows, 0, 5))
+river_params <- list('loc' = 865, 'shape' = 0.02, 'scale' = 11)
+river_hist <- revd(end_year-start_year+51, loc=river_params$loc, shape=river_params$shape, scale=river_params$scale, type='GEV')
 
 # loop over cases and estimate prior and posterior predictive distributions
 # register cluster
@@ -69,7 +80,7 @@ ncores <- detectCores()
 cl <- makeCluster(ncores)
 registerDoParallel(cl)
 
-hind_path <- file.path('../hind', paste0('model-id-', lik_type))
+hind_path <- file.path('hind', paste0('model-id-', lik_type))
 if (!dir.exists(hind_path)) {
   dir.create(hind_path)
 }
@@ -79,23 +90,19 @@ dists <- c('prior', 'posterior')
 hind_out <- foreach(i=1:length(dists),
               .packages=c('reticulate', 'pryr'),
               .export=c('elev', 'river_hist', 'run_model',
-                'nsamp', 'dists', 'yrs', 'rows', 'cols', 'prior', 'model',
+                'nsamp', 'dists', 'yrs', 'rows', 'cols', 'model', 'seed', 
                 'start_year', 'end_year', 'mem_length', 'out_path')
             ) %dopar% {
   # source Python model script
   # add examples and scripts directories to search path
-  py_run_string("import sys")
-  py_run_string(paste0("sys.path.append('",
-                  file.path(dirname(dirname(getwd())), 'examples', 'flood'),
-                  "')"
-                )
-  )
-  py_run_string(paste0("sys.path.append('",
-                  file.path(dirname(dirname(getwd())), 'scripts'),
-                  "')"
-               )
-  )
-                  
+  # add examples and scripts directories to search path
+  pysys <- import('sys')
+  functools <- import('functools')
+  
+  pysys$path <- c(pysys$path, file.path(dirname(dirname(getwd())), 'examples', 'flood'), 
+                  file.path(dirname(dirname(getwd())), 'scripts'))
+              
+  
   # source model file
   source_python(file.path(dirname(dirname(getwd())),
                 'examples','flood', paste0('flood_', model, '.py')))
@@ -105,7 +112,7 @@ hind_out <- foreach(i=1:length(dists),
   mod <- partial(BasicFloodModel,
               grid_sz=tuple(as.integer(cols), as.integer(rows)),
               mem_length=as.integer(mem_length),
-              seed=as.integer(1),
+              seed=as.integer(seed),
               river_hist=river_hist,
               elev=elev,
               query=dict('vac'=get_vac),
@@ -132,9 +139,9 @@ hind_out <- foreach(i=1:length(dists),
   }
 
   # load MCMC output
-    mcmc_file <- file.path(out_path, model, prior,
+    mcmc_file <- file.path(out_path, model,
     	                      paste('MCMC-', yrs, '-',
-           			            rows, '-', cols, '.rds',
+           			            rows, '-', cols, '-', seed, '.rds',
 	                          sep = ''
                           )
                   )
@@ -175,7 +182,7 @@ names(hind_out) <- dists
 print('Saving...')
 # set location and filename for bridge sampling output
 hind_file <- file.path(hind_path,
-  paste('hind-', yrs, '-', rows, '-', cols, '-', model, '-', prior, '.rds', sep=''))
+  paste('hind-', yrs, '-', rows, '-', cols, '-', model, '-', seed, '.rds', sep=''))
 
 saveRDS(hind_out, hind_file)
 
