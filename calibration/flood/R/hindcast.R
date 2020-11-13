06141f79a1f027045c245181dffa4beea7eb6275
vsrikrish <vxs914@psu.edu>
2020-11-13 10:02:18 -0500

    Increased font size

--- calibration/flood/R/hindcast.R
+++ calibration/flood/R/hindcast.R
@@ -1,7 +1,6 @@
 # This script plots hindcasts for the flood vacancy agent-based model
 
 library(ggplot2)  # plot functionality
-library(ncdf4)  # interface with netcdf files
 library(reshape2)
 library(RColorBrewer)
 library(ggpubr)
@@ -9,14 +8,16 @@ library(scales)
 
 # if PBS array used, get job number and specify parameters
 # otherwise get arguments from command call
+aid <- Sys.getenv('PBS_ARRAYID')
+seed <- as.numeric(aid)
+
 n_yrs <- c(10, 25, 50)
 n_rows <- c(5, 10)
 n_cols <- c(5, 10)
-priors <- c('informed', 'weakly')
 models <- c('simple', 'complex')
 lik_types <- c('agg', 'ind')
 
-pcases <- expand.grid(yr=n_yrs, cols=n_cols, rows=n_rows, prior=priors, model=models, lt=lik_types)
+pcases <- expand.grid(yr=n_yrs, cols=n_cols, rows=n_rows, model=models, lt=lik_types)
 pcases$agents <- pcases$rows*pcases$cols
 agents <- unique(pcases$agents)
 
@@ -33,14 +34,10 @@ for (model in models) {
     for (agent in as.character(agents)) {
       hind[[model]][[yr]][[agent]] <- vector('list', length(lik_types))
       names(hind[[model]][[yr]][[agent]]) <- lik_types
-      for (lt in lik_types) {
-        hind[[model]][[yr]][[agent]][[lt]] <- vector('list', length(priors))
-        names(hind[[model]][[yr]][[agent]][[lt]]) <- priors
-      }
+    
     }
   }
 }
-
 
 obs <- vector('list', length(models))
 names(obs) <- models
@@ -54,10 +51,6 @@ for (model in models) {
     for (agent in as.character(agents)) {
       obs[[model]][[yr]][[agent]] <- vector('list', length(lik_types))
       names(obs[[model]][[yr]][[agent]]) <- lik_types
-      for (lt in lik_types) {
-        obs[[model]][[yr]][[agent]][[lt]] <- vector('list', length(priors))
-        names(obs[[model]][[yr]][[agent]][[lt]]) <- priors
-      }
     }
   }
 }
@@ -68,7 +61,6 @@ for (i in 1:nrow(pcases)) {
   yrs <- pcases[i, 'yr']
   rows <- pcases[i, 'rows']
   cols <- pcases[i, 'cols']
-  prior <- pcases[i, 'prior']
   mod <- pcases[i, 'model']
   agents <- as.character(pcases[i, 'agents'])
   yr <- as.character(yrs)
@@ -82,50 +74,47 @@ for (i in 1:nrow(pcases)) {
     # load positive control data
     # load hindcasts
     hind_file <- file.path(hind_path,
-      paste('hind-', yrs, '-', rows, '-', cols, '-', mod, '-', prior, '.rds', sep=''))
+      paste('hind-', yrs, '-', rows, '-', cols, '-', mod, '-', seed, '.rds', sep=''))
 
     hc <- readRDS(hind_file)
 
     # compute quantiles and medians
-    hind[[mod]][[yr]][[agents]][[lik_type]][[prior]] <-
+    hind[[mod]][[yr]][[agents]][[lik_type]] <-
       lapply(hc, function(x) as.data.frame(t(apply(x, 1, quantile, probs=c(0.025, 0.5, 0.975)))))
 
-    hind[[mod]][[yr]][[agents]][[lik_type]][[prior]] <-
-      lapply(hind[[mod]][[yr]][[agents]][[lik_type]][[prior]], setNames, c('lower', 'median', 'upper'))
+    hind[[mod]][[yr]][[agents]][[lik_type]] <-
+      lapply(hind[[mod]][[yr]][[agents]][[lik_type]], setNames, c('lower', 'median', 'upper'))
 
-    hind[[mod]][[yr]][[agents]][[lik_type]][[prior]] <-
-      lapply(hind[[mod]][[yr]][[agents]][[lik_type]][[prior]], function(x) cbind(x, year=1:yrs))
+    hind[[mod]][[yr]][[agents]][[lik_type]] <-
+      lapply(hind[[mod]][[yr]][[agents]][[lik_type]], function(x) cbind(x, year=1:yrs))
 
     pcout_file <- file.path(data_path, 'model-id',
                           paste('pcout-', yrs, '-',
-                                  rows, '-', cols, '.nc',
+                                  rows, '-', cols, '-', seed, '.rds',
                                   sep = ''
                                )
                         )
 
-    f <- nc_open(pcout_file)
-    obs[[mod]][[yr]][[agents]][[lik_type]][[prior]] <- data.frame(obs=ncvar_get(f, 'vac'), year=1:yrs)
-    nc_close(f)
+    f <- readRDS(pcout_file)
+    obs[[mod]][[yr]][[agents]][[lik_type]] <- data.frame(obs=f$vac, year=1:yrs)
   }
 }
 
 hind.melt <- melt(hind, id.var=c('year', 'lower', 'median', 'upper'))
 obs.melt <- melt(obs, id.var=c('year', 'obs'))
 
-colnames(hind.melt) <- c('year', 'lower', 'median', 'upper', 'dist', 'prior', 'lik_type', 'parcels', 'years', 'model')
-colnames(obs.melt) <- c('year', 'obs', 'prior', 'lik_type', 'parcels', 'years', 'model')
+colnames(hind.melt) <- c('year', 'lower', 'median', 'upper', 'dist', 'lik_type', 'parcels', 'years', 'model')
+colnames(obs.melt) <- c('year', 'obs', 'lik_type', 'parcels', 'years', 'model')
 
 hind.melt$ymax <- as.numeric(hind.melt$parcels)
 hind.melt$parcels <- factor(hind.melt$parcels, levels(factor(hind.melt$parcels))[c(2,3,1)])
 hind.melt$years <- factor(hind.melt$years)
 hind.melt$model <- factor(hind.melt$model)
-hind.melt$prior <- factor(hind.melt$prior)
 hind.melt$dist <- factor(hind.melt$dist, c('prior', 'posterior'))
 
 obs.melt$parcels <- factor(obs.melt$parcels, levels(factor(obs.melt$parcels))[c(2,3,1)])
 obs.melt$years <- factor(obs.melt$years)
 obs.melt$model <- factor(obs.melt$model)
-obs.melt$prior <- factor(obs.melt$prior)
 
 colors <- brewer.pal(3, 'Dark2')
 colors[3] <- 'black'
@@ -138,21 +127,21 @@ for (i in 1:nrow(gcases)) {
   lt <- gcases[i, 'lt']
   mod <- gcases[i, 'models']
   
-  hind_sub <- hind.melt[(hind.melt$prior == 'informed') & (hind.melt$model == mod) & (hind.melt$lik_type == lt), ]
-  
+  hind_sub <- hind.melt[(hind.melt$model == mod) & (hind.melt$lik_type == lt) & (hind.melt$years != '10') & hind.melt$parcels != '100', ]
+  obs_sub <- obs.melt[(obs.melt$model == mod) & (obs.melt$lik_type == lt) & (obs.melt$years != '10') & obs.melt$parcels != '100', ]
   p[[i]] <- ggplot() +
       geom_ribbon(data=hind_sub,
         aes(x=year, ymin=lower, ymax=upper, fill=dist), alpha=0.2) +
       geom_line(data=hind_sub[hind_sub$dist == 'posterior',], aes(x=year, y=median, color=dist), size=1) +
-      geom_point(data=obs.melt, aes(x=year, y=obs, color='obs'), size=0.75, alpha=0.6) +
+      geom_point(data=obs_sub, aes(x=year, y=obs, color='obs'), size=0.75, alpha=0.6) +
       geom_blank(data=hind_sub, aes(x=0, y=ymax)) +
-      theme_bw(base_size=12) +
+      theme_bw(base_size=10) +
       scale_x_continuous('Simulation Year', breaks=pretty_breaks()) +
       scale_y_continuous('Housing Vacancies', breaks=pretty_breaks()) +
       scale_color_manual('', values=colors, labels=c('Observations', 'Posterior Median')) +
       scale_fill_manual('', values=colors[1:2], labels=c('Prior 95% CI', 'Posterior 95% CI')) +
       facet_grid(parcels ~ years, scales='free', labeller=label_both) +
-      theme(legend.position='bottom', legend.box='horizontal',
+      theme(legend.position='bottom', legend.box='vertical',
         panel.grid.major=element_blank(), panel.grid.minor=element_blank(), legend.spacing.y=unit(-0.5, 'lines'),
         panel.spacing.x=unit(0.4,'lines'),
         panel.spacing.y=unit(0.25, 'lines')
@@ -167,11 +156,7 @@ for (i in 1:nrow(gcases)) {
 
 q <- ggarrange(plotlist=p, ncol=2, nrow=2, heights=c(1, 1), widths=c(1, 1), labels='auto', font.label=list(size=12, color='black', face='bold'), legend='bottom', common.legend=TRUE)
 
-png('figures/hindcast-informed.png', width=8, height=8, units='in', res=300)
-print(q)
-dev.off()
-
-pdf('figures/hindcast-informed.pdf',
+pdf(paste0('figures/hindcast-', seed, '.pdf'),
   width=8, height=8)
 print(q)
 dev.off()
--- calibration/flood/R/plot_bf.R
+++ calibration/flood/R/plot_bf.R
@@ -17,7 +17,7 @@ names(bf) <- lt
 prior <- 'informed'
 
 compute_bf <- function(yrs, rows, cols, lik_type, prior) {
-  ml_path <- file.path('../ml', paste0('model-id-', lik_type)) # set path for ml output
+  ml_path <- file.path('ml', paste0('model-id-', lik_type)) # set path for ml output
   # load the ml output file and compute the bayes factor
   ml_file <- file.path(ml_path, paste('ml-', yrs, '-', rows, '-', cols, '-', prior, '.rds', sep=''))
   if (!file.exists(ml_file)) {return(NA)}
@@ -33,7 +33,7 @@ for (lik_type in lt) {
 }
 
 bf <- lapply(bf, function(x) cbind(x, agents=x$rows*x$cols))
-bf.melt <- melt(bf, id=c('years', 'rows', 'cols', 'agents'))
+bf.melt <- melt(bf[[1]], id=c('years', 'rows', 'cols', 'agents'))
 bf.melt$agents <- as.factor(bf.melt$agents)
 #bf.melt$years <- as.factor(bf.melt$years)
 colnames(bf.melt)[length(colnames(bf.melt))] <- 'lt'
@@ -45,31 +45,33 @@ lt_labels <- c('ind'='Individual Parcel', 'agg'='Aggregated')
 # plot
 p <- ggplot() +
   geom_point(data=bf.melt[bf.melt$agents != '25',],
-    aes(x=years, y=value, color=agents, shape=agents), size=2.5) +
-  geom_hline(data=bf.levels, aes(yintercept=val), linetype='dashed') +
-  geom_text(data=bf.levels, aes(y=(val+0.3)*1.25, x=40, label=lab), size=3) +
+    aes(x=years, y=value, color=agents, shape=agents), size=6) +
+  geom_hline(data=bf.levels, aes(yintercept=val), linetype='dashed', size=2) +
+  geom_text(data=bf.levels, aes(y=(val+0.35)*1.2, x=40, label=lab), size=6) +
   scale_y_log10('log-Bayes Factor', limits=c(0.1, 70),
     minor_breaks=c(0.5, 5, 50)) +
   scale_x_continuous('Years') +
   scale_color_brewer('Parcels', palette='Set1') +
   scale_shape('Parcels') +
-  theme_bw(base_size=12) +
+  theme_bw(base_size=20) +
   theme(legend.position='bottom',
         panel.grid.major=element_blank(),
-        panel.grid.minor=element_blank()) +
-  facet_wrap(vars(lt), nrow=2, labeller=labeller(lt=lt_labels), strip='left')
+        panel.grid.minor=element_blank(),
+	legend.margin=margin(t=0, unit='lines')) 
++
+  facet_wrap(vars(lt), nrow=1, labeller=labeller(lt=lt_labels), strip='left')
 
-pdf(paste0('figures/bf.pdf'), height=5, width=3)
-p
-dev.off()
+#pdf(paste0('figures/bf.pdf'), height=4, width=3)
+#p
+#dev.off()
 
-png(paste0('figures/bf.png'), height=5, width=3, units='in', res=300)
+png(paste0('figures/poster/bf.png'), height=6.5, width=10, units='in', res=600)
 p
 dev.off()
 
 #png(file.path('figures', 'poster', 'bf.png'), height=6.5, width=13, units='in', res=300)
 #p
 #dev.off()
-th=13, units='in', res=300)
+#th=13, units='in', res=300)
 #p
 #dev.off()
